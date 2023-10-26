531e30f8fc9bdeff142fa63bb5e6adfdacde3b08
Horev Vladimir <vshorev@yandex.ru>
2023-10-26 18:53:53 +0300

    запилил соритровку частично

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -8,28 +8,40 @@ import { ImCross } from "react-icons/im";
 
 function Basket(props) {
 
-    const [renderinput, setrenderinput] = useState();
+    let a=props.basket
 
+    let b=[...new Set(props.basket)]
+    const [renderinput, setrenderinput] = useState(a);
+
+
+    const price = () => {
+    console.log(a)
+    console.log(b)
+    };
 
     return (
         <div className="basketDisplay scrollbasket">
             <div className='d-flex justify-content-between  header'> <p className='mx-4 mt-4'>Оформление заказа</p>   <ImCross className='mx-4 mt-4' onClick={props.handleClosebasket} />       </div>
 
 
-            {props.basket.map((item) => {
+            {b.map((item) => {
+
+                if(2==2){ 
                 return (
                     <div>
                         <div className='d-flex mt-3  ' >
-                            <img src={item.image} className="cursor cardstyle  border  border-4 border-white mx-3" alt="..." />
+                            <img onClick={price} src={item.image} className="cursor cardstyle  border  border-4 border-white mx-3" alt="..." />
                             <p> {item.name}   </p>
                             <p> {item.price}   </p>
                             <p> 3шт   </p>
+                            <p> количество </p>
                             <ImCross className='mx-4 mt-4' />
                         </div>
                         <hr className='border border-4 border-white mx-3' ></hr>
                     </div>
                 )
-            })}
+            }
+            })} 
 
 
             <button className='transformtext mx-4  '> <span> Оформить заказ</span></button>
