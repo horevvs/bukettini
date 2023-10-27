de86118f8b4867ae28efde7fca543c7fc0aba4b1
Horev Vladimir <vshorev@yandex.ru>
2023-10-27 14:39:29 +0300

    2

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -19,12 +19,33 @@ function Basket(props) {
         }, 0)
 
 
+var result = {};
+
+
+
+// arr.forEach(function(a){
+//     if (result[a.id] != undefined)
+//         ++result[a.id];
+//     else
+//         result[a.id] = 1;
+// });
+//  console.log(result)
+// ;
+
+
+        const sum=price.forEach(function(a){
+                if (result[a.id] != undefined)
+                    ++result[a.id];
+                else
+                    result[a.id] = 1;
+            });
+             console.log(result)
 
 
     return (
         <div className="basketDisplay scrollbasket">
             <div className='d-flex justify-content-between  header'> <p className='mx-4 mt-4'>Оформление заказа</p>    <div>Итого  {totalAmount}  рублей</div>  <ImCross className='mx-4 mt-4' onClick={props.handleClosebasket} />       </div>
-            {price.map((item) =>  {
+            {b.map((item) =>  {
 
                 return (
 
@@ -34,7 +55,7 @@ function Basket(props) {
                             <img src={item.image} className="cursor cardstyle  border  border-4 border-white mx-3" alt="..." />
                             <p> {item.name}   </p>
                             <p> {item.price}   </p>
-                            <p> количество   {   }  </p>
+                            <p>   количество - {result[item.id]  }  </p>
                               
 
                             <ImCross className='mx-4 mt-4' />
