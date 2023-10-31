420660fbc4716732372bb4c0e40e3b013e9ccb96
Vladimir horev <vshorev@yandex.ru>
2023-10-31 17:33:04 +0700

    готов к первому кастингу тимлида

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -2,6 +2,8 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ImCross } from "react-icons/im";
+import { useState, } from 'react';
+import Modal from 'react-bootstrap/Modal';
 
 function Basket(props) {
 
@@ -25,6 +27,15 @@ function Basket(props) {
     })
 
 
+    const [show, setShow] = useState(false);
+
+    const openmodalmenu = () => {
+
+        setShow(!show)
+    };
+
+
+
     return (
         <div className="basketDisplay scrollbasket">
             <div className='d-flex justify-content-between  header'>
@@ -43,7 +54,7 @@ function Basket(props) {
                                 <div className='mx-2'> {item.name}   </div>
                                 <div className='mx-2'> цена: {item.price} руб. </div>
                                 <button type="button" className="btn btn-dark btn-sm mx-2 px-2" onClick={() => props.deleteminus(item.id)}> - </button>
-                                  количество - {result[item.id]} шт.
+                                количество - {result[item.id]} шт.
                                 <button type="button" className="btn btn-dark btn-sm mx-2 px-2" onClick={() => props.deletplus(item.id)}> + </button>
                             </div>
 
@@ -56,7 +67,46 @@ function Basket(props) {
                     </div>
                 )
             })}
-            <button className='transformtext mx-4'> <span> Оформить заказ</span></button>
+            <button className='transformtext mx-4' onClick={openmodalmenu} > <span> Оформить заказ</span></button>
+            <Modal show={show}>
+                <Modal.Header className='bg-dark bg-opacity-25' onClick={openmodalmenu} closeButton>Отправить  заявку </Modal.Header>
+                <Modal.Body>
+                    <>
+
+                        {b.map((item) => {
+                            return (
+                                <div key={item.id}>
+                                    <div className='d-flex mt-3   justify-content-between ' >
+                                        <div className='mx-2'> {item.name}   </div>
+                                        <div >количество - {result[item.id]} шт.</div>
+                                    </div>
+                                    <hr className='border border-4 border-dark mx-3' ></hr>
+                                </div>
+                            )
+                        })}
+                        <form>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Как вас зовут уважаемый/ая?</label>
+                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            </div>
+                            <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Адрес доставки</label>
+                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            </div>
+                            <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label"> Ваш номер, куда позвонить?</label>
+                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            </div>
+                            <button type="submit" class="btn btn-dark">Отправить заявку</button>
+                        </form>
+
+                    </>
+                </Modal.Body>
+                <Modal.Footer className='bg-dark bg-opacity-75'>
+                    <div className=''>Итого  {totalAmount}  рублей</div>
+
+                </Modal.Footer>
+            </Modal>
         </div>
     )
 }
