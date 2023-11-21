370a4d5526c1936cc85ac5c17e49a52761057ca7
Horev Vladimir <vshorev@yandex.ru>
2023-11-21 15:16:04 +0300

    1

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -161,83 +161,88 @@ function MainComponent() {
 
 
     return (
-  
-          
-        
+
+
+
         <div className="App">
 
-{loading ? (
+            {loading ? (
                 <div className='bodyloader '><span className="loader">  <img src={MainPageLogotip} width='100px' height='100px' alt='' />    </span>
                 </div>
-            ) : ( <>  
-            <div className="header d-flex   .d-md-none  justify-content-between">
-                <img src={MainPageLogotip} width='80px' height='80px' className='mt-3 mx-3 ' alt='' />
-                <span className='mt-4  text-center fs-3 d-none  d-sm-block'> Bouquet of food  - лучший подарок человеку, у которого есть все! </span>
-               
-                <div className='basket'>
-                    <div> <SlBasket className='mx-4 my-5  ' />
-                        <span className=' mx-3  '>
-                            {basket.length === 0 ? (null) : (<div onClick={handleClosebasket} className='digital text-dark bg-white'>  {basket.length} </div>)}</span>
-                    </div>
-                    <div className={baskethide ? "baskethide" : " "}  >
-                        <Basket basket={basket} handleClosebasket={handleClosebasket} deleteFrombasket={deleteFrombasket} deleteminus={deleteminus} deletplus={deletplus} />
+            ) : (<>
+                <div className="header d-flex   .d-md-none  justify-content-between">
+                    <img src={MainPageLogotip} width='80px' height='80px' className='mt-3 mx-3 ' alt='' />
+                    <span className='mt-4  text-center fs-3 d-none  d-sm-block'> Bouquet of food  - лучший подарок человеку, у которого есть все! </span>
+
+                    <div className='basket'>
+                        <div> <SlBasket className='mx-4 my-5  ' />
+                            <span className=' mx-3  '>
+                                {basket.length === 0 ? (null) : (<div onClick={handleClosebasket} className='digital text-dark bg-white'>  {basket.length} </div>)}</span>
+                        </div>
+                        <div className={baskethide ? "baskethide" : " "}  >
+                            <Basket basket={basket} handleClosebasket={handleClosebasket} deleteFrombasket={deleteFrombasket} deleteminus={deleteminus} deletplus={deletplus} />
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='mainbackground   '>
-                <div className='d-flex flex-wrap justify-content-around  p-3 '>
-                
-                    <button onClick={handleClick2} className='transformtext mt-2'> <span> Весь список</span></button>
-                    {groups.map((item) => {
-                        return (
-                            <div key={item.id} className=' carts  mt-2 ' >
-                                <div className=" ">
-                                    <button onClick={() => findItemsByIdAndRefreshState(item.id)} className='transformtext '> <span>   {item.name}</span></button>
-                                </div>
-                            </div>
-                        )
-                    })}
+                <div className='mainbackground   '>
+                    <div className='d-flex flex-wrap justify-content-around  p-3 '>
 
-                    <button onClick={FilterdBasket} className='transformtext  mt-2  '><span>Сортировать по цене</span></button>
-                    <button onClick={handleClick} className='transformtext  mt-2 '><span>Обратная связь</span></button>
-                </div>
-
-                <div className='d-flex  flex-wrap scroll justify-content-around  '>
-                    {state.map((item) => {
-                        return (
-                            <div key={item.id} className='mb-2 carts justify-content-center' >
-                                <img onClick={() => handleShow(item.id)} src={item.image} className=" cardstyle cursor  border  border-5 border-white" alt="..." />
-                                <div className=' sas  bg-dark bg-opacity-75  d-flex flex-column  justify-content-center align-items-center '>
-                                    <div className='border border-white text-white  bg-dark boxbutton text-center pt-2' onClick={() => addtoBasket(item.id)}>
-                                        <p className="font-weight-bold">  <SlBasket /> Добавить в корзину</p>
-                                    </div>
-                                    <div className=' border border-white text-dark   bg-white boxbutton text-center mt-5 pt-2' onClick={() => handleShow(item.id)}>
-                                        <p className="font-weight-bold"> <BsSearch className='mx-2' /> Просмотр</p>
+                        <button onClick={handleClick2} className='transformtext mt-2'> <span> Весь список</span></button>
+                        {groups.map((item) => {
+                            return (
+                                <div key={item.id} className=' carts  mt-2 ' >
+                                    <div className=" ">
+                                        <button onClick={() => findItemsByIdAndRefreshState(item.id)} className='transformtext '> <span>   {item.name}</span></button>
                                     </div>
                                 </div>
-                                <Modal show={show[item.id]}>
-                                    <Modal.Header className='bg-dark bg-opacity-25' onClick={() => handleClose(item.id)} closeButton>
-                                    <p className='mt-2'> {item.name} </p>
-                                    </Modal.Header>
-                                    <img src={item.image} className="img-fluid cursor border  border-5 border-white" alt="..." />
-                                    <Modal.Footer className='bg-dark bg-opacity-25'>
-                                        <p> Стоимость- {item.price} рублей</p>
-                                    </Modal.Footer>
-                                </Modal>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
+                            )
+                        })}
 
-            <div className="footer d-flex justify-content-between">
+                        <button onClick={FilterdBasket} className='transformtext mt-2'><span>Сортировать по цене</span></button>
+                        <button onClick={handleClick} className='transformtext  mt-2'><span>Обратная связь</span></button>
+                    </div>
+                    <div className={Filterbasket ? "baskethide " : "  d-flex justify-content-center m-2 "} >
+                        <input type="text" value={inputsmin} placeholder='от' class="form-control fs-5  w-25" onChange={(e) => setInputsmin(e.target.value)} />
+                        <input type="text" value={inputsmax} placeholder='до' class="form-control fs-5 mx-2 w-25" onChange={(e) => setInputsmax(e.target.value)} />
+                        <button type="button" onClick={() => sortByprice(inputsmax, inputsmin)} class="btn btn-dark mx-2">Сортировать</button>
+                    </div>
 
-                <div className=" d-flex justify-content-between mx-4   ">
-                    <a className='px-3 text-white  ' href="" > <SlSocialVkontakte /></a>
-                    <a className='px-3 text-white ' href="" > <BsWhatsapp /></a>
+                    <div className='d-flex  flex-wrap scroll justify-content-around  '>
+                        {state.map((item) => {
+                            return (
+                                <div key={item.id} className='mb-2 carts flex-wrap justify-content-center' >
+                                    <img onClick={() => handleShow(item.id)} src={item.image} className=" cardstyle cursor  border  border-5 border-white" alt="..." />
+                                    <div className=' sas  bg-dark bg-opacity-75  d-flex flex-column  justify-content-center align-items-center '>
+                                        <div className='border border-white text-white  bg-dark boxbutton text-center pt-2' onClick={() => addtoBasket(item.id)}>
+                                            <p className="font-weight-bold">  <SlBasket /> Добавить в корзину</p>
+                                        </div>
+                                        <div className=' border border-white text-dark   bg-white boxbutton text-center mt-5 pt-2' onClick={() => handleShow(item.id)}>
+                                            <p className="font-weight-bold"> <BsSearch className='mx-2'/> Просмотр</p>
+                                        </div>
+                                    </div>
+                                    <Modal show={show[item.id]}>
+                                        <Modal.Header className='bg-dark bg-opacity-25' onClick={() => handleClose(item.id)} closeButton>
+                                            <p className='mt-2'> {item.name} </p>
+                                        </Modal.Header>
+                                        <img src={item.image} className="img-fluid cursor border  border-5 border-white" alt="..." />
+                                        <Modal.Footer className='bg-dark bg-opacity-25'>
+                                            <p> Стоимость- {item.price} рублей</p>
+                                        </Modal.Footer>
+                                    </Modal>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
-            </div>
+
+                <div className="footer d-flex justify-content-between">
+
+                    <div className=" d-flex justify-content-between mx-4   ">
+                        <a className='px-3 text-white  ' href="" > <SlSocialVkontakte /></a>
+                        <a className='px-3 text-white ' href="" > <BsWhatsapp /></a>
+                    </div>
+                </div>
             </>)}
         </div>
     )
